a927090babaf96d635f92f88320b67cf687f4049
Edgar M <egarmola08@gmail.com>
2026-08-12 10:26:17 -0600

    feat: user has many invoices migrations

--- djinvoice/invoice/models.py
+++ djinvoice/invoice/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
 class Invoice(models.Model):
     invoice_number = models.CharField(max_length=20)
-    customer_name = models.CharField(max_length=100)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     due_date = models.DateField()
     status = models.CharField(max_length=20, choices=[('paid', 'Paid'), ('unpaid', 'Unpaid')])
 
     def __str__(self):
-        return f"Invoice {self.invoice_number} - {self.customer_name}"
+        return f"Invoice {self.invoice_number} - {self.customer.email}"
